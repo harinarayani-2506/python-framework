84a86f0920e925b56e4f0d43f929ab5dd9cafd77
harinarayani <harinivenkat2412@gmail.com>
2025-01-09 15:14:32 +0530

    Update web_automation.py

--- web_automation.py
+++ web_automation.py
@@ -8,28 +8,3 @@ driver = webdriver.Chrome()
 driver.get("https://www.linkedin.com/")
 driver.implicitly_wait(3)
 driver.maximize_window()
-input_element = WebDriverWait(driver, 60).until(
-    EC.presence_of_element_located((By.XPATH, "//a[@data-test-id='home-hero-sign-in-cta']"))
-)
-input_element.click()
-email_input_element = WebDriverWait(driver, 60).until(
-    EC.visibility_of_element_located((By.XPATH, "//input[@id='username']"))
-)
-email_input_element.click()
-email_input_element.send_keys("harinivenkat24@gmail.com")
-entered_value = email_input_element.get_property("value")
-print(f'Entered email is : {entered_value}')
-
-pwd_input_element = WebDriverWait(driver, 60).until(
-    EC.visibility_of_element_located((By.XPATH, "//input[@id='password']"))
-)
-pwd_input_element.click()
-pwd_input_element.send_keys("testing")
-entered_value = pwd_input_element.get_property("value")
-print(f'Entered password is : {pwd_input_element}')
-
-driver.execute_script('window.scrollTo(0,document.body.scrollHeight);')
-signin_button : WebElement = driver.find_element(By.XPATH, "//button[@class='btn__primary--large from__button--floating']")
-signin_button.click()
-
-driver.close()
