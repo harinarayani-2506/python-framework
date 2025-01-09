193ac1432137f3027501b5e0843f34a1d6b2a98a
harinarayani <harinivenkat2412@gmail.com>
2025-01-09 15:00:49 +0530

    Update web_automation.py

--- web_automation.py
+++ web_automation.py
@@ -1,10 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.common.keys import Keys
-import time
-from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
 driver.get("https://www.linkedin.com/")
